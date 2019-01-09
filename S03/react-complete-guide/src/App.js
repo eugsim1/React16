f82d56090a5fa40f622c2d198cefbfeb64f9cd1c
eugsim1 <eug@no.com>
2019-01-09 07:45:28 +0100

    JSX 2

--- S03/react-complete-guide/src/App.js
+++ S03/react-complete-guide/src/App.js
@@ -1,28 +1,70 @@
-// import react and react class
-///
-// see the code of return much easier
 import React, { Component } from 'react';
-
 import './App.css';
-import logo from './logo.svg';
+import Person from './Person/Person';
 
-
-// create a javascript calss
-// render class to render
 class App extends Component {
-    render() {
-         return (
-             <div className = "App">
-            <h1> Hi i m a react application </h1> 
-            <p>This is really working</p>
-            </div>
-        ); 
-     // all previous line are compile to the code below ...
-     // class name shoud be used instead of class   
-     //   return React.createElement('div', null, 'h1', 'Hi ......');
-     // one root element /component
-    }
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState( {
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    } )
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    } )
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
 }
 
-// export class default export  => import App
 export default App;
